cd8e575909360bbf85d68d8954282f8847b6c2b9
LoganBrownGU <2641407b@student.gla.ac.uk>
2025-05-14 12:27:17 +0100

    Added radial-realtime

---
+++ Team1/radar-testing/radial-realtime/src/device.rs
@@ -0,0 +1,85 @@
+use std::{f32::consts::PI, fs::File, io::{stdin, stdout, Read, Write}, sync::mpsc, thread};
+use std::str;
+
+
+static CALIBRATION_FILE: &str = "calibration";
+
+pub fn parse_line(file: &mut File) -> (f32, f32, f32, f32) {
+    let mut buf = [0u8; 1024];
+    
+    let _ = file.read(&mut buf).unwrap_or(0);
+    let line = match str::from_utf8(&buf) {
+        Ok(s) => s,
+        Err(_) => return (0.0, 0.0, 0.0, 0.0),
+    };
+
+    let parts: Vec<&str> = line.split(",").collect();
+    if parts.len() != 14 { return (0.0, 0.0, 0.0, 0.0) }
+    let distance = parts[4..].iter().map(|s| s.parse::<f32>().unwrap_or(f32::MAX)).fold(f32::MAX, |a, f| (if a < f {a} else {f}));
+    
+    let mag_x = parts[0].parse::<f32>().unwrap_or(0.0);
+    let mag_y = parts[1].parse::<f32>().unwrap_or(0.0);
+    let mag_z = parts[2].parse::<f32>().unwrap_or(0.0);
+
+    (distance / 1000.0, mag_x, mag_y, mag_z)
+}
+
+pub fn reuse_calibration() -> (f32, f32, f32, f32) {
+    let mut file = File::open(CALIBRATION_FILE).unwrap();
+    let mut buf: Vec::<u8> = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+
+    let content = match str::from_utf8(&buf) {
+        Ok(s) => s,
+        Err(_) => return (0.0, 0.0, 0.0, 0.0),
+    };
+
+    let parts: Vec<f32> = content.split(" ").map(|s| {s.parse::<f32>().unwrap()}).collect();
+
+    (parts[0], parts[1], parts[2], parts[3])
+}
+
+pub fn calibrate(file: &mut File) -> (f32, f32, f32, f32) {
+    let mut readings: Vec<(f32, f32, f32, f32)> = Vec::new();
+
+    let (tx, rx) = mpsc::channel();
+
+    let listener = thread::spawn(move || {
+        let mut input = String::new();
+        print!("Press enter to stop calibration: ");
+        let _ = stdout().flush();
+        let _ = stdin().read_line(&mut input);
+        let _ = tx.send(true);
+    });
+
+    while !(rx.try_recv().unwrap_or_else(|_| false)) {
+        readings.push(parse_line(file));
+    }
+    let _ = listener.join();
+
+    // (max x, min x, max y, min y)
+    let mut calibration = (f32::MIN, f32::MAX, f32::MIN, f32::MAX);
+    for reading in readings {
+        if reading.1 > calibration.0 { calibration.0 = reading.1 }
+        if reading.1 < calibration.1 { calibration.1 = reading.1 }
+        if reading.2 > calibration.2 { calibration.2 = reading.2 }
+        if reading.2 < calibration.3 { calibration.3 = reading.2 }
+    }
+
+    let mut out_file = File::create(CALIBRATION_FILE).unwrap();
+    out_file.write(format!("{} {} {} {}", calibration.0, calibration.1, calibration.2,
+                           calibration.3).as_bytes()).unwrap();
+
+    calibration
+}
+
+pub fn normalise_mag(reading: f32, max: f32, min: f32) -> f32 {
+    (reading - min) / (max - min) - 0.5
+}
+
+pub fn calculate_heading(mag_x: f32, mag_y: f32, calibration: (f32, f32, f32, f32)) -> f32 {
+    let norm_x = normalise_mag(mag_x, calibration.0, calibration.1);
+    let norm_y = normalise_mag(mag_y, calibration.2, calibration.3);
+
+    norm_y.atan2(norm_x) + PI
+}
---
+++ Team1/radar-testing/radial-realtime/src/main.rs
@@ -0,0 +1,144 @@
+use std::{collections::VecDeque, env::{self}, fs::File, marker::PhantomData, sync::mpsc::{self, Receiver}, thread};
+use std::io::{stdin, stdout, Write};
+use nannou::{color::{encoding, Alpha}, prelude::*};
+use crate::device::reuse_calibration;
+
+mod device;
+
+#[derive(Debug)]
+pub struct Ping {
+    pub distance: f32,
+    pub heading: f32
+}
+
+static MAX_PINGS: usize = 2000;
+
+struct Model {
+    rx: Receiver<Ping>,
+    pings: VecDeque<Ping>,
+    zoom: f32,
+}
+
+fn polar_to_cartesian(theta: f32, r: f32) -> Vec2 {
+    vec2(theta.cos() * r, theta.sin() * r)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if File::open(args[1].as_str()).is_err() {
+        println!("Failed to open {}. Check that the path is correct and the device is plugged in.",
+                 args[1]);
+        return;
+    }
+
+    let app = nannou::app(model)
+        .event(event)
+        .update(update)
+        .simple_window(view)
+        .size(1000, 1000);
+
+    app.run();
+        
+}
+
+fn model(_app: &App) -> Model {
+    let args: Vec<String> = env::args().collect();
+
+    let fp = args[1].clone();
+    let mut file = File::open(fp).unwrap();
+
+    print!("Recalibrate? (y/n): ");
+    stdout().flush().unwrap();
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let calibration = if input.trim().eq("y") { device::calibrate(&mut file) } else {
+        reuse_calibration() };
+
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        loop {
+            let (distance, mag_x, mag_y, _) = device::parse_line(&mut file);
+            let ping = Ping{ distance, heading: device::calculate_heading(mag_x, mag_y,
+                                                                          calibration)};
+            if ping.heading.is_nan() { continue; }
+            let _ = tx.send(ping);
+        }
+    });
+
+    Model { rx, pings: VecDeque::new(), zoom: 5.0 }
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {
+    match _event {
+        Event::WindowEvent{ id: _, simple } => {simple.inspect(|e| {
+            match e {
+                KeyPressed(k) => match k {
+                    Key::Return => _model.pings.clear(),
+                    Key::Up => _model.zoom -= 0.5,
+                    Key::Down => _model.zoom += 0.5,
+                    _ => (),    
+                },
+                _ => ()
+            }
+        }); ()},
+        _ => ()
+    }
+
+    if _model.zoom < 0.5 { _model.zoom = 0.5 }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+    let mut ping = _model.rx.try_recv();
+
+    while ping.is_ok() {
+        _model.pings.push_back(ping.unwrap());
+        ping = _model.rx.try_recv();
+        if _model.pings.len() > MAX_PINGS { _model.pings.pop_front(); }
+    }
+}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    let width = app.window_rect().right().min(app.window_rect().top());
+    let scale = width / _model.zoom;
+
+    draw.background().color(BLACK);
+
+    for d in 1..=5 {
+        draw.ellipse().no_fill().radius((d as f32) * scale).stroke(WHITE).stroke_weight(1.0);
+        draw.text(d.to_string().as_str()).color(WHITE).x((d as f32) * scale + 10.0);
+    }
+
+    for d in 1..=9 {
+        draw.ellipse().no_fill().radius((d as f32) * 0.5 * scale).stroke(GREY).stroke_weight(0.5);
+    }
+
+    if !_model.pings.is_empty() {
+        let alpha_scale = 1.0 / _model.pings.len() as f32;
+        let points = _model.pings.iter().enumerate().map(|(i, p)| {
+            let v = polar_to_cartesian(p.heading, p.distance) * scale;
+            (vec2(v.x, v.y), Alpha { color: Rgb { red: alpha_scale * i as f32, blue: alpha_scale
+                * (_model.pings.len() - i) as f32, green: 0.0, standard:
+            PhantomData::<encoding::Srgb> }, alpha: alpha_scale * i as f32 })
+        });
+
+        if points.len() != 0 {
+            draw.point_mode().polyline().points_colored(points);
+        }
+    }
+
+    _model.pings.iter().last().inspect(|ping| {
+        draw.arrow().color(BLUE)
+            .start(vec2(0.0, 0.0))
+            .end(polar_to_cartesian(ping.heading, width))
+            .head_length(7.0)
+            .head_width(7.0);
+        draw.text(format!("{:.2}", ping.distance).as_str()).color(WHITE).x(app.window_rect().left() +
+            20.0).y(app.window_rect().top() - 10.0);
+    });
+
+
+    draw.to_frame(app, &frame).unwrap();
+}
