9e245590cd487c824f6afa600a862308902df454
LoganBrownGU <2641407b@student.gla.ac.uk>
2025-05-14 12:14:55 +0100

    Realtime working again

--- Team1/radar-testing/realtime/realtime.py
+++ Team1/radar-testing/realtime/realtime.py
@@ -13,7 +13,6 @@ args = ap.parse_args()
 
 # ── serial ────────────────────────────────────────────────────────
 ser = serial.Serial(args.port, args.baud, timeout=0.2)
-csv  = re.compile(r"^\s*(\d+),(\d+),(\d+),(\d+)")
 
 # ── plotting boiler-plate ─────────────────────────────────────────
 plt.style.use("fast")
@@ -37,32 +36,24 @@ for i in range(5):
     lines.append(line)
 ax.legend(fontsize="small", loc="upper left")
 
-dists_now = []
-time_now = -1
-
 # ── update func ───────────────────────────────────────────────────
 def update(_):
-    global dists_now
-    global time_now
     global dists
 
     while ser.in_waiting:
-        m=csv.match(ser.readline().decode("ascii","ignore"))
-        if not m: continue
-        p,d,s,t = int(m[1]),int(m[2]),int(m[3]),int(m[4])
-        if len(times) == 0: times.append(t)
+        m=ser.readline().decode("ascii","ignore").split(",")
+        if len(m) != 14: continue
+        dists_now = list(map(float, m[4:8]))
+
+        if len(times) == 0:
+            times.append(int(m[3]))
+        times.append(int(m[3]))
+
+        sorted_dists = np.sort(dists_now)
+        for d, sd in zip(dists, sorted_dists):
+            if sd == 1e8: d.append(d[-1])
+            else:         d.append(sd / 1000.0)
         
-        if time_now != t and len(dists_now) != 0: 
-            sorted_dists = np.sort(dists_now)
-            for i in range(len(dists)):
-                if i < len(sorted_dists): dists[i].append(sorted_dists[i]); since_update[i] = 0
-                else: dists[i].append(dists[i][-1]); since_update[i] += 1
-            times.append(t)
-
-            dists_now = []
-            time_now = t
-
-        dists_now.append(d / 1000.0)
 
     for i in range(len(dists)):
         if since_update[i] >= keep: 
