1d32aed26acd8d5df937f183ac342364237bf70f
bj-allen <benallen331@gmail.com>
2025-05-16 12:02:59 +0100

    updated transmitter for Teensy 4.1 SPI1

--- Team4/EndToEndTransmission/Transmitter/Transmitter.ino
+++ Team4/EndToEndTransmission/Transmitter/Transmitter.ino
@@ -1,14 +1,15 @@
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <RHHardwareSPI1.h>
 
-#define RFM95_CS 10
-#define RFM95_RST 9
-#define RFM95_INT 2
+#define RFM95_CS 38 // arduino pin 10
+#define RFM95_RST 29 // arduino pin 9
+#define RFM95_INT 28 // arduino pin 2
 
 #define RF95_FREQ 868.0
 
 // Singleton instance of the radio driver
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
+RH_RF95 rf95(RFM95_CS, RFM95_INT, hardware_spi1);
 
 // Constants for the simulation
 #define UPDATE_INTERVAL 100  // ms between sensor readings (10Hz)
@@ -60,7 +61,7 @@ void setup() {
     while (1);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  
+
   // trying to force LoRa mode:
   rf95.spiWrite(0x01, 0x81);
 
@@ -75,7 +76,7 @@ void setup() {
   rf95.setSpreadingFactor(10);
   // Set transmitter power
   rf95.setTxPower(23, false);
-  
+
   // Initialize random seed for simulation (lets us have a little bit of consistency)
   randomSeed(analogRead(0));
 }
@@ -88,17 +89,17 @@ void getIMUData(SensorData *data) {
   static float baseAccelX = 0.0;
   static float baseAccelY = 0.0;
   static float baseAccelZ = 9.8; // Starting with approximate gravity
-  
+
   basePitch += random(-5, 6) / 10.0;
   baseRoll += random(-5, 6) / 10.0;
   baseYaw += random(-10, 11) / 10.0;
-  
+
   // Simulate acceleration with some random drift
   baseAccelX = 0.7 * baseAccelX + random(-20, 21) / 100.0;
   baseAccelY = 0.7 * baseAccelY + random(-20, 21) / 100.0;
   baseAccelZ = 0.95 * baseAccelZ + 0.05 * 9.8 + random(-10, 11) / 100.0;
-  
-  // Keep within bounds (I'm assuming our firefighter is probably not upside down) 
+
+  // Keep within bounds (I'm assuming our firefighter is probably not upside down)
   if (basePitch > 45.0) basePitch = 45.0;
   if (basePitch < -45.0) basePitch = -45.0;
   if (baseRoll > 45.0) baseRoll = 45.0;
@@ -106,7 +107,7 @@ void getIMUData(SensorData *data) {
   // Yaw can be 0-360
   if (baseYaw >= 360.0) baseYaw -= 360.0;
   if (baseYaw < 0.0) baseYaw += 360.0;
-  
+
   // Keep accelerations within reasonable bounds
   if (baseAccelX > 3.0) baseAccelX = 3.0;
   if (baseAccelX < -3.0) baseAccelX = -3.0;
@@ -114,7 +115,7 @@ void getIMUData(SensorData *data) {
   if (baseAccelY < -3.0) baseAccelY = -3.0;
   if (baseAccelZ > 12.0) baseAccelZ = 12.0;
   if (baseAccelZ < 7.0) baseAccelZ = 7.0;
-  
+
   data->pitch = basePitch;
   data->roll = baseRoll;
   data->yaw = baseYaw;
@@ -129,7 +130,7 @@ void getRadarData(SensorData *data) {
   // For this I'll make the distance vary based on yaw
   float baseDistance = 100.0 + 50.0 * sin(data->yaw * PI / 180.0);
   float randomVariation = random(-10, 11);
-  
+
   data->radarDistance = baseDistance + randomVariation;
 }
 
@@ -138,13 +139,13 @@ void sendData(SensorData data) {
   char accelXStr[8], accelYStr[8], accelZStr[8];
   char seqHex[4]; // 3 hex chars + null terminator
   char dataPacket[256]; // Increased size for additional data
-  
+
   // Increment sequence number and wrap around
   sequenceNumber = (sequenceNumber + 1) & MAX_SEQUENCE;
-  
+
   // Convert sequence number to 3-digit hex
   sprintf(seqHex, "%03X", sequenceNumber);
-  
+
   // Convert floats to strings manually
   dtostrf(data.pitch, 6, 2, pitchStr);
   dtostrf(data.roll, 6, 2, rollStr);
@@ -156,23 +157,23 @@ void sendData(SensorData data) {
 
   // Format data as a simple string
   // Format: "SEQ:{seq},P:{pitch},R:{roll},Y:{yaw},D:{distance},AX:{accelX},AY:{accelY},AZ:{accelZ},T:{timestamp}"
-  sprintf(dataPacket, "SEQ:%s,P:%s,R:%s,Y:%s,D:%s,AX:%s,AY:%s,AZ:%s,T:%lu", 
-          seqHex, pitchStr, rollStr, yawStr, distanceStr, 
+  sprintf(dataPacket, "SEQ:%s,P:%s,R:%s,Y:%s,D:%s,AX:%s,AY:%s,AZ:%s,T:%lu",
+          seqHex, pitchStr, rollStr, yawStr, distanceStr,
           accelXStr, accelYStr, accelZStr, data.timestamp);
-  
+
   int packetLength = strlen(dataPacket);
-  
-  // Serial.print("Sending data: "); 
+
+  // Serial.print("Sending data: ");
   // Serial.println(dataPacket);
-  
+
   // Send the packet
   rf95.send((uint8_t *)dataPacket, packetLength);
   rf95.waitPacketSent();
-  
+
   // Wait for a reply (acknowledgment)
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
-  
+
   if (rf95.waitAvailableTimeout(500)) {
     if (rf95.recv(buf, &len)) {
       // Serial.print("Got reply: ");
@@ -189,17 +190,17 @@ void sendData(SensorData data) {
 
 void loop() {
   SensorData currentData;
-  
+
   // Get the current time
   currentData.timestamp = millis();
-  
+
   // Get simulated sensor readings
   getIMUData(&currentData);
   getRadarData(&currentData);
-  
+
   // Send the data
   sendData(currentData);
-  
+
   // Wait before the next reading
   delay(UPDATE_INTERVAL);
 }
